d86e6da1fd1aaa641f95283805c4c343c7253862
Guide Fari <guideg6@gmail.com>
2021-03-31 09:21:40 +0200

    using 'as' keyword (cast operator) in ts file

---
+++ Chapter02/todoit-v1-initial/todo-it.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("just do it");
+var todoList = [];
+var todoInput = document.getElementById('todoInput');
+// console.log('current todo list:', todoList)
---
+++ Chapter02/todoit-v1-initial/todo-it.ts
@@ -0,0 +1,6 @@
+console.log("just do it"); 
+
+const todoList: string[] = []
+const todoInput: HTMLInputElement = document.getElementById('todoInput') as HTMLInputElement
+
+// console.log('current todo list:', todoList)
