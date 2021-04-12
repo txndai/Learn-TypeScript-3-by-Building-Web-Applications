7cedd7c7b8b8af1561fe706d730ad78cb3add604
Guide Fari <guideg6@gmail.com>
2021-04-12 22:58:29 +0200

    enum & abstract class done

--- Chapter04/mediaman-v1-initial/mediaman.ts
+++ Chapter04/mediaman-v1-initial/mediaman.ts
@@ -1,3 +1,62 @@
-console.log("MediaMan - Loading...");
+enum Genre {
+    Horror = "Horror", 
+    Fantastic = "Fantastic", 
+    Thriller = "Thriller", 
+    Romance = "Romance", 
+    Fiction = "Fiction"
+}
 
-console.log("MediaMan loaded");
+abstract class Media {
+    private _identifier: string
+
+    protected constructor(
+        private _name: string,
+        private _description: string,
+        private _pictureLocation: string,
+        private _genre: Genre,
+        identifier?: string,
+    ) {
+        if (identifier) {
+            this._identifier = identifier
+        } else {
+            // this is just for the example; for any real project, use 
+            // UUIDs instead: https://www.npmjs.com/package/uuid 
+            this._identifier = Math.random().toString(36).substr(2,9); 
+        }
+    }
+
+    get identifier(): string {
+        return this._identifier
+    }
+    set identifier(identifier: string) { 
+        this._identifier = identifier; 
+    }
+
+    get name(): string { 
+        return this._name; 
+    }
+    set name(name: string) { 
+        this._name = name; 
+    }
+
+    get description(): string { 
+        return this._description; 
+    }
+    set description(description: string) { 
+        this._description = description; 
+    } 
+
+    get pictureLocation(): string { 
+        return this._pictureLocation; 
+    } 
+    set pictureLocation(pictureLocation: string) { 
+        this._pictureLocation = pictureLocation; 
+    } 
+
+    get genre(): Genre { 
+        return this._genre; 
+    } 
+    set genre(genre: Genre) { 
+        this._genre = genre; 
+    }
+}
