0ceabb2cde9ad5a51e9b2cd77c01136fdd572a5d
Guide Fari <guideg6@gmail.com>
2021-04-13 16:31:45 +0200

    add: media collection class

--- Chapter04/mediaman-v1-initial/mediaman.ts
+++ Chapter04/mediaman-v1-initial/mediaman.ts
@@ -126,3 +126,64 @@ class Movie extends Media {
         this._duration = duration; 
     }
 }
+
+class MediaCollection<T extends Media> {
+    private _identifier: string
+    private _name: string = ""
+    private _collection: ReadonlyArray<T> = []
+    private readonly _type: Function
+
+    constructor(
+        type: Function,
+        name?: string,
+        identifier?: string
+    ) {
+            this._type = type
+
+            if(name) {
+                this._name = name
+            }
+            if (identifier) {
+                this._identifier = identifier
+            } else {
+                // this is just for the example; for any real project, use 
+                // UUIDs instead: https://www.npmjs.com/package/uuid 
+                this._identifier = Math.random().toString(36).
+                substr(2, 9);
+            }
+        }
+
+    get identifier(): string { 
+        return this._identifier; 
+    } 
+    set identifier(identifier: string) { 
+        this._identifier = identifier; 
+    } 
+    
+    get name(): string { 
+        return this._name; 
+    } 
+    set name(name: string) { 
+        this._name = name; 
+    } 
+    
+    get collection(): ReadonlyArray<T> { 
+        return this._collection; 
+    } 
+    set collection(collection: ReadonlyArray<T>) { 
+        this._collection = collection; 
+    } 
+    
+    addMedia(media: Readonly<T>): void { 
+        if (media) { 
+            this._collection = this._collection.concat(media); 
+        } 
+    } 
+    removeMedia(itemId: string) { 
+        if (itemId) { 
+            this._collection = this._collection.filter(item => { 
+                return item.identifier !== itemId; 
+            }); 
+        } 
+    } 
+}
